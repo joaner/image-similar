731753c5468abc01dd93e31181026b120628d261
xiaokang pu <pxk@seiue.com>
2018-08-16 23:51:10 +0800

    create src

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 gcc \
     -I/usr/local/Cellar/glib/2.56.1/include/glib-2.0 \
     -L/usr/local/Cellar/glib \
-    -lglib-2.0 -lgd main.c -o image-similar && ./image-similar test/chrome_48.png test/chrome_32.png
+    -lglib-2.0 -lgd src/main.c -o image-similar \
+    && ./image-similar test/chrome_48.png test/chrome_32.png
 
